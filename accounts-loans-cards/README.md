e8246a0f759fcb06c8bdefe65d1028aaaef5db94
qinxhannah <qin.xia@northeastern.edu>
2022-06-13 19:09:38 -0700

    Built accounts microservice

---
+++ accounts-loans-cards/README.md
@@ -0,0 +1,3 @@
+### Created three microservices specific to Accounts, Loans, and Cards
+
+Description: This repository has three maven projects with the names of accounts, loans, and cards. These three projects will act as microservices and these are built by taking XiaohanBank as an example Bank application.
---
+++ accounts-loans-cards/accounts/src/main/java/com/xiaohan/accounts/controller/AccountsController.java
@@ -0,0 +1,31 @@
+package com.xiaohan.accounts.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.xiaohan.accounts.model.Accounts;
+import com.xiaohan.accounts.model.Customer;
+import com.xiaohan.accounts.repository.AccountsRepository;
+
+
+@RestController
+public class AccountsController {
+
+  @Autowired
+  private AccountsRepository accountsRepository;
+
+  @PostMapping("/myAccount")
+  public Accounts getAccountDetails(@RequestBody Customer customer) {
+
+    Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
+    if (accounts != null) {
+      return accounts;
+    } else {
+      return null;
+    }
+
+  }
+
+}
