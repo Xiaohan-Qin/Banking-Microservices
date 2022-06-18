76588fe28ee79fb12f7701f3994e4ec5fbb0c3d8
qinxhannah <qin.xia@northeastern.edu>
2022-06-18 16:03:33 -0700

    Made Microservices resilient using Resilience4j patterns

--- accounts-loans-cards/accounts/src/main/java/com/xiaohan/accounts/controller/AccountsController.java
+++ accounts-loans-cards/accounts/src/main/java/com/xiaohan/accounts/controller/AccountsController.java
@@ -22,6 +22,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 public class AccountsController {
@@ -61,6 +64,8 @@ public class AccountsController {
 
 
   @PostMapping("/myCustomerDetails")
+  @CircuitBreaker(name = "detailsForCustomerSupportApp",fallbackMethod="myCustomerDetailsFallBack")
+  @Retry(name = "retryForCustomerDetails", fallbackMethod = "myCustomerDetailsFallBack")
   public CustomerDetails myCustomerDetails(@RequestBody Customer customer) {
     Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
     List<Loans> loans = loansFeignClient.getLoansDetails(customer);
@@ -73,6 +78,26 @@ public class AccountsController {
 
     return customerDetails;
 
+  }
+
+  private CustomerDetails myCustomerDetailsFallBack(Customer customer, Throwable t) {
+    Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
+    List<Loans> loans = loansFeignClient.getLoansDetails(customer);
+    CustomerDetails customerDetails = new CustomerDetails();
+    customerDetails.setAccounts(accounts);
+    customerDetails.setLoans(loans);
+    return customerDetails;
+
+  }
+
+  @GetMapping("/sayHello")
+  @RateLimiter(name = "sayHello", fallbackMethod = "sayHelloFallback")
+  public String sayHello() {
+    return "Hello, Welcome to XiaohanBank";
+  }
+
+  private String sayHelloFallback(Throwable t) {
+    return "Hi, Welcome to XiaohanBank";
   }
 
 }
